1ae3cd4b30a4208f23abfc82063e559d5628221f
kiyotakeshi-m1 <ponkan@kiyotamitsunozominoMacBook-Air.local>
2021-05-16 22:50:29 +0900

    fake store api から値を取得し表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from 'react-query';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// components
+import Item from './Item/Item';
+import { Drawer, LinearProgress } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
+
+// styles
+import { Wrapper } from './App.styles';
+
+// types
+export type CartItemType = {
+    id: number;
+    category: string;
+    description: string;
+    image: string;
+    price: number;
+    title: string;
+    amount: number;
+};
+
+// {
+//   "id":1
+//   ,"title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
+//   ,"price":109.95
+//   ,"description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday"
+//   ,"category":"men's clothing"
+//   ,"image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+// }
+const getProducts = async (): Promise<CartItemType[]> =>
+    await (await fetch('https://fakestoreapi.com/products')).json();
+
+const App = () => {
+    const { data, isLoading, error } = useQuery<CartItemType[]>(
+        'products',
+        getProducts
+    );
+    console.log(data);
+
+    const getTotalItems = () => null;
+    const handleAddToCart = (clickedItem: CartItemType) => null;
+    const handleRemoveFromCart = () => null;
+
+    if (isLoading) return <LinearProgress />;
+    if (error) return <div>Something went wrong ...</div>;
+
+    return (
+        <Wrapper>
+            <Grid container spacing={3}>
+                {data?.map((item) => (
+                    <Grid item key={item.id} xs={12} sm={4}>
+                        <Item item={item} handleAddToCart={handleAddToCart} />
+                    </Grid>
+                ))}
+            </Grid>
+        </Wrapper>
+    );
+};
 
 export default App;
