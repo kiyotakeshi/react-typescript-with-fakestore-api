aef236f9200cac447936bf787ca406a2c98181e2
kiyotakeshi-m1 <ponkan@kiyotamitsunozominoMacBook-Air.local>
2021-05-18 09:26:46 +0900

    ショッピングカートに追加したアイテムの数と合計金額を表示するよう実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { useQuery } from 'react-query';
+import { useState } from 'react';
 
 // components
 import Item from './Item/Item';
-import { Drawer, LinearProgress } from '@material-ui/core';
+import Cart from './Cart/Cart';
+
+import { Badge, Drawer, LinearProgress } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 
 // styles
-import { Wrapper } from './App.styles';
+import { Wrapper, StyledButton } from './App.styles';
 
 // types
 export type CartItemType = {
@@ -32,21 +35,75 @@ const getProducts = async (): Promise<CartItemType[]> =>
     await (await fetch('https://fakestoreapi.com/products')).json();
 
 const App = () => {
+    const [cartOpen, setCartOpen] = useState(false);
+    const [cartItems, setCartItems] = useState([] as CartItemType[]);
     const { data, isLoading, error } = useQuery<CartItemType[]>(
         'products',
         getProducts
     );
-    console.log(data);
+    // console.log(data);
 
-    const getTotalItems = () => null;
-    const handleAddToCart = (clickedItem: CartItemType) => null;
-    const handleRemoveFromCart = () => null;
+    const getTotalItems = (items: CartItemType[]) =>
+        items.reduce((ack: number, item) => ack + item.amount, 0);
+
+    const handleAddToCart = (clickedItem: CartItemType) => {
+        setCartItems((prev) => {
+            console.log(clickedItem);
+            console.log(prev);
+
+            // already in the cart??
+            const isItemInCart = prev.find(
+                (item) => item.id === clickedItem.id
+            );
+
+            if (isItemInCart) {
+                return prev.map((item) =>
+                    item.id === clickedItem.id
+                        ? { ...item, amount: item.amount + 1 }
+                        : item
+                );
+            }
+
+            // first time the item is added
+            return [...prev, { ...clickedItem, amount: 1 }];
+        });
+    };
+    const handleRemoveFromCart = (id: number) => {
+        setCartItems((prev) =>
+            prev.reduce((ack, item) => {
+                if (item.id === id) {
+                    // 1つだけなら array から削除(array を返す)
+                    if (item.amount === 1) return ack;
+                    return [...ack, { ...item, amount: item.amount - 1 }];
+                } else {
+                    return [...ack, item];
+                }
+            }, [] as CartItemType[])
+        );
+    };
 
     if (isLoading) return <LinearProgress />;
     if (error) return <div>Something went wrong ...</div>;
 
+    // TODO: 続きから https://youtu.be/sfmL6bGbiN8?t=2233
     return (
         <Wrapper>
+            <Drawer
+                anchor="right"
+                open={cartOpen}
+                onClose={() => setCartOpen(false)}
+            >
+                <Cart
+                    cartItems={cartItems}
+                    addToCart={handleAddToCart}
+                    removeFromCart={handleRemoveFromCart}
+                />
+            </Drawer>
+            <StyledButton onClick={() => setCartOpen(true)}>
+                <Badge badgeContent={getTotalItems(cartItems)} color="error">
+                    <AddShoppingCartIcon />
+                </Badge>
+            </StyledButton>
             <Grid container spacing={3}>
                 {data?.map((item) => (
                     <Grid item key={item.id} xs={12} sm={4}>
